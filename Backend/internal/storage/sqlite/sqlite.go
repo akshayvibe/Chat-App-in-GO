1d78b765f7706c4a3dccadafab01c3f7b586a52c
akshay <akshayjha2111@gmail.com>
2025-12-13 17:53:57 +0530

    updating the sqlite and adding CreateConnection

--- Backend/internal/storage/sqlite/sqlite.go
+++ Backend/internal/storage/sqlite/sqlite.go
@@ -1,22 +1,49 @@
 package sqlite
 
 import (
-	"database/sql"
+	// "database/sql"
+	"errors"
 
 	"github.com/akshayjha21/Chat-App-in-GO/internal/config"
+	"github.com/akshayjha21/Chat-App-in-GO/internal/types"
+	"gorm.io/driver/sqlite" // Sqlite driver based on GGO
+	"gorm.io/gorm"
 	// "golang.org/x/tools/go/analysis/passes/defers"
 )
 
 type Sqlite struct {
-	Db *sql.DB
+	Db *gorm.DB
 }
 
-func New(cfg *config.Config)(*Sqlite,error){
-	db,err:=sql.Open("sqlite",cfg.StoragePath)
+func New(cfg *config.Config) (*Sqlite, error) {
+	db, err := gorm.Open(sqlite.Open(cfg.StoragePath), &gorm.Config{})
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-	defer db.Close()
-	db.Exec(`
-	`)
+	err = db.AutoMigrate(
+		&types.User{},
+		&types.Room{},
+		&types.RoomMember{},
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &Sqlite{Db: db}, nil
+}
+func (s *Sqlite) CreateConnection(user *types.User, room *types.Room) (*types.RoomMember, error) {
+
+	newMember := types.RoomMember{
+		RoomId: room.Id,
+		UserId: user.Id,
+		Role:   "member",
+	}
+	result := s.Db.Create(&newMember)
+	if result.Error != nil {
+		// Specifically check for unique constraint error (user already in room)
+		if errors.Is(result.Error, gorm.ErrDuplicatedKey) {
+			return nil, errors.New("user already in this room")
+		}
+		return nil, result.Error
+	}
+	return &newMember, nil
 }
--- Backend/internal/types/types.go
+++ Backend/internal/types/types.go
@@ -3,9 +3,9 @@ package types
 import "time"
 
 type User struct {
-	Id        uint   `json:"Id" gorm:"primaryKey"`
-	Name      string `json:"Name" validate:"required"`
-	Email     string `json:"Email" gorm:"unique;not null"`
+	Id        uint      `json:"Id" gorm:"primaryKey"`
+	Name      string    `json:"Name" validate:"required"`
+	Email     string    `json:"Email" gorm:"unique;not null"`
 	CreatedAt time.Time `json:"created_at"`
 }
 type Room struct {
@@ -14,7 +14,7 @@ type Room struct {
 	Isprivate bool   `json:"Is_private" gorm:"default:false"`
 }
 type RoomMember struct {
-	RoomId uint32 `json:"RoomID" gorm:"primaryKey"`
-	UserId uint32 `json:"UserID" gorm:"primaryKey"`
+	RoomId uint   `json:"RoomID" gorm:"primaryKey"`
+	UserId uint   `json:"UserID" gorm:"primaryKey"`
 	Role   string `json:"Role" gorm:"default:'member'"`
 }
